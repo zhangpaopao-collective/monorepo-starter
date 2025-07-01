4d129c18ac60b2e229ba61214fdbe9b98ba4dc50
zhangpaopao <zhangpaopao0609@gmail.com>
2025-07-01 12:26:40 +0800

    feat: init nice

---
+++ packages/common/utils.ts
@@ -0,0 +1,11 @@
+import dayjs from 'dayjs';
+
+/**
+ * Format a given date to a specific format string using day.js
+ * @param date - The date to be formatted, can be a Date object, timestamp, or string
+ * @param format - The format string, default is 'YYYY-MM-DD HH:mm:ss'
+ * @returns The formatted date string
+ */
+export function formatDate(date: dayjs.ConfigType, format: string = 'YYYY-MM-DD HH:mm:ss'): string {
+  return dayjs(date).format(format);
+}
